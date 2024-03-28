5e889835b8ff4eb53f916ed3f296bc2bc1213f61
PolinaQAe <138627297+PolinaQAe@users.noreply.github.com>
2024-03-28 15:57:13 +0300

    Add files via upload

---
+++ seasonfirst.java
@@ -0,0 +1,25 @@
+public class seasonfirst {
+
+        public static void main (String[] args) {
+
+            int day = 5;
+            switch (day) {
+                case 1,2,12:
+                    System.out.println("Winter");
+                    break;
+                case 3,4,5:
+                    System.out.println("Spring");
+                    break;
+                case 6,7,8:
+                    System.out.println("Summer");
+                    break;
+                case 9,10,11:
+                    System.out.println("Autumn");
+                    break;
+
+            }
+
+        }
+    }
+
+
